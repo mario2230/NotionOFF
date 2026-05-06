cb485995306d1952d3fb9c0525b7609ef1274e5a
mario2230 <124641210+mario2230@users.noreply.github.com>
2026-05-06 13:41:33 -0300

    feat(telas): Criando as tela e definindo rota somente front

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,27 +5,31 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/pages/home'
   },
   {
-    path: '/tabs/',
+    path: '/pages/login',
+    component: () => import('@/views/LoginPage.vue')
+  },
+  {
+    path: '/pages/cadastro',
+    component: () => import('@/views/CadastroPage.vue')
+  },
+  {
+    path: '/pages/',
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirect: '/tabs/tab1'
+        path: 'home',
+        component: () => import('@/views/HomePage.vue')
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: 'perfil',
+        component: () => import('@/views/PerfilPage.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'documentos',
+        component: () => import('@/views/DocumentosPage.vue')
       }
     ]
   }
--- src/views/DocumentosPage.vue
+++ src/views/DocumentosPage.vue
@@ -2,22 +2,24 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Documentos</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
+          <ion-title size="large">Documentos</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ExploreContainer name="Tab 3 page" />
+      <ion-card>
+        <ion-checkbox>Check Box</ion-checkbox>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCheckbox, IonCard  } from '@ionic/vue';
+
+
 </script>
